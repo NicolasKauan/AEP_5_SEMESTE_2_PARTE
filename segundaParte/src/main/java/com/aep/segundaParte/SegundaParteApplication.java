798582bde365b1cb0aadd79723c9fd0d05c793c2
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-06-07 16:21:05 -0300

    Criação do repostirio com spring boot

---
+++ segundaParte/src/main/java/com/aep/segundaParte/SegundaParteApplication.java
@@ -0,0 +1,13 @@
+package com.aep.segundaParte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaParteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaParteApplication.class, args);
+	}
+
+}
---
+++ segundaParte/src/test/java/com/aep/segundaParte/SegundaParteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aep.segundaParte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundaParteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
